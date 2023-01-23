4d72021d45d2e3175c4754876b3c05957edf12e3
vinay1033 <108928213+vinay1033@users.noreply.github.com>
2023-01-23 16:57:21 +0530

    Coding Club NMIET

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(window).bind('scroll',function(){
+        var gap=50;
+        if($(window).scrollTop()>gap){
+            $('.navigation').addClass('nav_b')
+        }else{
+            $('.navigation').removeClass('nav_b')
+        }  
+});
+});
+
+
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 300) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
